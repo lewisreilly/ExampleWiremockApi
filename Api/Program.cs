f0e719baa3515f6efdbbe4d1c4b63780244139fe
Lewis Reilly <lewisreilly@outlook.com>
2023-01-30 22:40:22 +0000

    Add solution

---
+++ Api/Program.cs
@@ -0,0 +1,56 @@
+﻿using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace Api
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = WireMockServer.StartWithAdminInterface(8080);
+            Console.WriteLine("API is running at http://localhost:{0}", string.Join(",", server.Ports));
+
+            server
+                .Given(Request.Create()
+                    .WithPath("/echo")
+                    .UsingGet()
+                    )
+                .RespondWith(Response.Create()
+                    .WithSuccess()
+                    .WithBodyAsJson("Your path is {{request.path}}.")
+                    .WithTransformer());
+
+            server
+                .Given(Request.Create()
+                    .WithPath("/healthcheck")
+                    .UsingGet()
+                    )
+                .RespondWith(Response.Create()
+                    .WithSuccess()
+                    .WithBodyAsJson($"Current time: {DateTime.Now}"));
+
+            server
+                .Given(Request.Create()
+                    .WithPath("/slow_response")
+                    .UsingGet()
+                    )
+                .RespondWith(Response.Create()
+                    .WithSuccess()
+                    .WithBodyAsJson($"Current time: {DateTime.Now}")
+                    .WithDelay(TimeSpan.FromSeconds(1)));
+
+            var paths = server.Mappings.Select(m => m.Path).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine("Available methods:");
+            Console.WriteLine("  /echo");
+            Console.WriteLine("  /healthcheck");
+            Console.WriteLine("  /slow_response");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ExampleWiremockApi
-Example of using Wiremock.NET to create a simple API for use with testing.
+
+Example of using [Wiremock.NET](https://github.com/WireMock-Net/WireMock.Net/wiki) to create a simple, local API for returning stubbed responses.
+
+Supported requests
+- GET /echo - echos back the URL path
+- GET /healthcheck - responds with the current date and time
+- GET /slow_response - same behaviour as /healthcheck after an initial delay
